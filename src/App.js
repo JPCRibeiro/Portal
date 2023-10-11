47de2bb6386f52c34be956234bbf71c643c76f80
JPCRibeiro <jopedroribeiro9@gmail.com>
2023-10-11 16:15:41 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import './CssGlobal.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/login/Login"
 import Inicio from "./pages/inicio/Inicio";
+import Requerimento from "./pages/requerimento/Requerimento";
+import Biblioteca from "./pages/biblioteca/Biblioteca";
 import Boletim from "./pages/boletim/Boletim";
 
 export default function App() {
@@ -10,6 +13,8 @@ export default function App() {
           <Route exact path="/" element={<Login/>}/>
           <Route path="/inicio" element={<Inicio/>}/>
           <Route path="/boletim" element={<Boletim/>}/>
+          <Route path="/requerimento" element={<Requerimento/>}/>
+          <Route path="/biblioteca" element={<Biblioteca/>}/>
         </Routes>
     </Router>
   );
